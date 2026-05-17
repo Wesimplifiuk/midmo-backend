ad73b0b9bcde335a3aa601880527318484b7c7bb
Roman salom <romansalom@MacBook-Pro-de-Roman.local>
2026-05-17 10:34:40 -0300

    test commit hubspot contacts

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('Bike API running')
+})
+
+app.get('/test-hubspot', async (req, res) => {
+
+  try {
+
+    const response = await axios.get(
+      'https://api.hubapi.com/crm/v3/objects/contacts?limit=1',
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.HUBSPOT_TOKEN}`,
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return res.json({
+      success: true,
+      data: response.data
+    })
+
+  } catch (error) {
+
+    console.log(error.response?.data || error.message)
+
+    return res.status(500).json({
+      success: false,
+      error: error.response?.data || error.message
+    })
+
+  }
+
 })
 
 const PORT = process.env.PORT || 3000
