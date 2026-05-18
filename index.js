19ac7ea5c14d416284cbfa8fb9e18d5c66f07468
Roman salom <romansalom@MacBook-Pro-de-Roman.local>
2026-05-18 15:09:39 -0300

    create bike custom object

--- index.js
+++ index.js
@@ -194,14 +194,14 @@ app.post('/vehicle', async (req, res) => {
 
 // CREATE BIKE FROM HUBSPOT FORM
 
+// CREATE BIKE + BUSCAR SU ID
+
 app.post('/create-bike', async (req, res) => {
 
   try {
 
     const {
-
       email,
-
       vehicle_registration,
       make,
       variant,
@@ -210,139 +210,148 @@ app.post('/create-bike', async (req, res) => {
       year,
       colour,
       mot
-
     } = req.body
 
-    const response =
-      await axios.post(
+    // 1. SUBMIT FORM (crea el objeto y lo asocia al contacto)
 
-        'https://api-eu1.hsforms.com/submissions/v3/integration/submit/26636031/f01310fa-918d-4909-9502-2f6f387d4212',
-
-        {
-
-   fields: [
-
-  {
-    name:
-      'email',
-    value:
-      email
-  },
-
-  {
-    name:
-      '2-202877425/vehicle_registration',
-    value:
-      vehicle_registration
-  },
-
-  {
-    name:
-      '2-202877425/make',
-    value:
-      make
-  },
-
-  {
-    name:
-      '2-202877425/variant',
-    value:
-      variant
-  },
-
-  {
-    name:
-      '2-202877425/engine_size',
-    value:
-      engine_size
-  },
-
-  {
-    name:
-      '2-202877425/mileage',
-    value:
-      mileage
-  },
-
-  {
-    name:
-      '2-202877425/year',
-    value:
-      year
-  },
-
-  {
-    name:
-      '2-202877425/colour',
-    value:
-      colour
-  },
-
-  {
-    name:
-      '2-202877425/mot',
-
-    value:
-      Array.isArray(mot)
-        ? mot.join(';')
-        : mot
-  }
-
-],
-
-          context: {
-
-            pageUri:
-              'https://thomasjamesbromley.wixstudio.com',
-
-            pageName:
-              'Bike Form'
-
-          }
-
-        },
-
-        {
-          headers: {
-            'Content-Type':
-              'application/json'
-          }
+    await axios.post(
+      'https://api-eu1.hsforms.com/submissions/v3/integration/submit/26636031/f01310fa-918d-4909-9502-2f6f387d4212',
+      {
+        fields: [
+          { name: 'email', value: email },
+          { name: '2-202877425/vehicle_registration', value: vehicle_registration },
+          { name: '2-202877425/make', value: make },
+          { name: '2-202877425/variant', value: variant },
+          { name: '2-202877425/engine_size', value: engine_size },
+          { name: '2-202877425/mileage', value: mileage ? String(mileage) : '0' },
+          { name: '2-202877425/year', value: year },
+          { name: '2-202877425/colour', value: colour },
+          { name: '2-202877425/mot', value: Array.isArray(mot) ? mot.join(';') : mot }
+        ],
+        context: {
+          pageUri: 'https://thomasjamesbromley.wixstudio.com',
+          pageName: 'Bike Form'
         }
+      },
+      {
+        headers: { 'Content-Type': 'application/json' }
+      }
+    )
 
-      )
+    // 2. BUSCAR EL OBJETO RECIEN CREADO POR vehicle_registration
+
+    const searchResponse = await axios.post(
+      'https://api.hubapi.com/crm/v3/objects/2-202877425/search',
+      {
+        filterGroups: [
+          {
+            filters: [
+              {
+                propertyName: 'vehicle_registration',
+                operator: 'EQ',
+                value: vehicle_registration
+              }
+            ]
+          }
+        ],
+        sorts: [
+          {
+            propertyName: 'createdate',
+            direction: 'DESCENDING'
+          }
+        ],
+        limit: 1
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + process.env.HUBSPOT_API_KEY
+        }
+      }
+    )
+
+    const results = searchResponse.data.results
+
+    const bikeId = results && results.length > 0
+      ? results[0].id
+      : null
 
     return res.json({
-
       success: true,
-      response:
-        response.data
-
+      bikeId: bikeId
     })
 
   } catch (error) {
 
     console.log(
-      JSON.stringify(
-        error.response?.data,
-        null,
-        2
-      )
+      JSON.stringify(error.response?.data, null, 2)
     )
 
     return res.status(500).json({
-
       success: false,
-
-      error:
-        error.response?.data ||
-        error.message
-
+      error: error.response?.data || error.message
     })
 
   }
 
 })
 
+
+// UPDATE BIKE (STEP 3)
+
+app.post('/update-bike', async (req, res) => {
+
+  try {
+
+    const {
+      bikeId,
+      motorcycle_condition,
+      do_you_have_the_keys_,
+      do_you_have_the_v5c_,
+      do_you_know_how_much_you_are_looking_for_,
+      when_are_you_looking_to_sell_your_bike,
+      bike_owner_postal_code
+    } = req.body
+
+    const response = await axios.patch(
+      'https://api.hubapi.com/crm/v3/objects/2-202877425/' + bikeId,
+      {
+        properties: {
+          motorcycle_condition: motorcycle_condition,
+          do_you_have_the_keys_: do_you_have_the_keys_,
+          do_you_have_the_v5c_: do_you_have_the_v5c_,
+          do_you_know_how_much_you_are_looking_for_: do_you_know_how_much_you_are_looking_for_,
+          when_are_you_looking_to_sell_your_bike: when_are_you_looking_to_sell_your_bike,
+          bike_owner_postal_code: bike_owner_postal_code
+        }
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + process.env.HUBSPOT_API_KEY
+        }
+      }
+    )
+
+    return res.json({
+      success: true,
+      data: response.data
+    })
+
+  } catch (error) {
+
+    console.log(
+      JSON.stringify(error.response?.data, null, 2)
+    )
+
+    return res.status(500).json({
+      success: false,
+      error: error.response?.data || error.message
+    })
+
+  }
+
+})
 
 
 
