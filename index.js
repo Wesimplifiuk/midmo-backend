fce4e8bd727b1915cd6e4cd4271a5b922684c7d0
Roman salom <romansalom@MacBook-Pro-de-Roman.local>
2026-05-18 11:39:03 -0300

    fix vehicle endpoint

--- index.js
+++ index.js
@@ -1,3 +1,91 @@
+const express = require('express')
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+// ROOT
+
+app.get('/', (req, res) => {
+
+  res.send('Bike API running')
+
+})
+
+// SEARCH CONTACT
+
+app.post('/search-contact', async (req, res) => {
+
+  try {
+
+    const {
+      email,
+      firstname,
+      lastname,
+      phone
+    } = req.body
+
+    const response = await axios.post(
+      'https://api-eu1.hsforms.com/submissions/v3/integration/submit/26636031/f66ee250-4c4e-4666-8f1e-b8deefe3afab',
+      {
+        fields: [
+          {
+            name: 'email',
+            value: email
+          },
+          {
+            name: 'firstname',
+            value: firstname
+          },
+          {
+            name: 'lastname',
+            value: lastname
+          },
+          {
+            name: 'phone',
+            value: phone
+          }
+        ],
+        context: {
+          pageUri: 'https://motortradeteam.com',
+          pageName: 'Motorbike Lead Form'
+        }
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+    )
+
+    return res.json({
+      success: true,
+      hubspot: response.data
+    })
+
+  } catch (error) {
+
+    console.log(
+      error.response?.data ||
+      error.message
+    )
+
+    return res.status(500).json({
+      success: false,
+      error:
+        error.response?.data ||
+        error.message
+    })
+
+  }
+
+})
+
+// VEHICLE ENDPOINT
+
 app.post('/vehicle', async (req, res) => {
 
   try {
@@ -11,36 +99,7 @@ app.post('/vehicle', async (req, res) => {
       registration
     )
 
-    // VEHICLE DATA GLOBAL REQUEST
-
-    const response = await axios.post(
-
-      'YOUR-VEHICLE-ENDPOINT-HERE',
-
-      {
-        registrationNumber:
-          registration
-      },
-
-      {
-        headers: {
-
-          'Content-Type':
-            'application/json',
-
-          Authorization:
-            `Bearer ${process.env.VEHICLE_API_KEY}`
-
-        }
-      }
-
-    )
-
-    console.log(
-      response.data
-    )
-
-    // MAP VEHICLE RESPONSE
+    // TEMP MOCK RESPONSE
 
     return res.json({
 
@@ -48,23 +107,12 @@ app.post('/vehicle', async (req, res) => {
 
       vehicle: {
 
-        make:
-          response.data.make || '',
-
-        model:
-          response.data.model || '',
-
-        variant:
-          response.data.variant || '',
-
-        year:
-          response.data.year || '',
-
-        engineSize:
-          response.data.engineSize || '',
-
-        colour:
-          response.data.colour || ''
+        make: 'Yamaha',
+        model: 'MT-07',
+        variant: 'ABS',
+        year: '2021',
+        engineSize: '689cc',
+        colour: 'Black'
 
       }
 
@@ -72,21 +120,26 @@ app.post('/vehicle', async (req, res) => {
 
   } catch (error) {
 
-    console.log(
-      error.response?.data ||
-      error.message
-    )
+    console.log(error)
 
     return res.status(500).json({
 
       success: false,
-
-      error:
-        error.response?.data ||
-        error.message
+      error: error.message
 
     })
 
   }
+
+})
+
+const PORT =
+  process.env.PORT || 3000
+
+app.listen(PORT, () => {
+
+  console.log(
+    `Server running on ${PORT}`
+  )
 
 })
